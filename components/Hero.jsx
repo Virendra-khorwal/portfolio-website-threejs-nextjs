b38a6978ebd36daca070fa5473071511cd35f716
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-07-21 21:14:23 +0530

    update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,34 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import { styles } from "@/app/style";
+import { ComputerCanvas } from "@/components/canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto ">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-start gap-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violent-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            👋 Hi, I'm <span className="text-[#915eff]">Virendra</span>{" "}
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I'm a <span className="text-[#915eff]">Full Stack Web </span>and
+            <span className="text-[#915eff]"> Android Developer </span>
+            and <span className="text-[#915eff]">UI/UX Designer</span>.
+          </p>
+        </div>
+      </div>
+      <ComputerCanvas />
+    </section>
+  );
+};
 
 export default Hero;
