69245d62c7f9abdf4db2e0319d06ba3207951c85
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-07-30 23:24:46 +0530

    hero section mobile responsive add

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -27,6 +27,17 @@ const Hero = () => {
         </div>
       </div>
       <ComputerCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div 
+              animate={{ y: [0, 24, 0] }}
+              transition={{ duration: 1.5, repeat: Infinity, repeatType: "loop" }}
+              className="w-[10px] h-[10px] bg-secondary rounded-full mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
