fd08ad136f8bc53dcd1a8ec2c48707d6a3ba5099
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-07-31 22:33:44 +0530

    minor ui fix

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-1 sm:h-60 h-40 violet-gradient" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
@@ -27,7 +27,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputerCanvas />
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-8 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div 
